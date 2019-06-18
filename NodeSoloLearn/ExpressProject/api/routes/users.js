da10ef44fb484cb358dbf33e497220a7ff8d711f
Cabomaldade <soutom@yahoo.com.br>
2019-06-17 23:07:50 -0300

    Lindo - Tudo funcionando - Update - Delete - Full CRUD

--- NodeSoloLearn/ExpressProject/api/routes/users.js
+++ NodeSoloLearn/ExpressProject/api/routes/users.js
@@ -5,44 +5,69 @@ const User = require('../models/user');
 const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
+    User.find()          // If I dont pass an argument, it will find all the elements  
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json(docs);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    /*res.status(200).json({ //usado no aprendizado
         message: 'Handling GET request of the /users'
-    });
+    });*/
 });
 
 router.post('/', (req, res, next) => {
     const user = new User({
         _id: new mongoose.Types.ObjectId(), // aqui usa mongoose para criar um ID
         name: req.body.name,
-        age: req.body.age
+        age: req.body.age,
+        country: req.body.country,
+        email: req.body.email
     });
-    user.save().then(result => { // aqui adiciona uma promisse
-        console.log(result);
-    })
-        .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'Handling POST request of the /users',
-        user: user
-    });
+    user
+        .save()
+        .then(result => { // aqui adiciona uma promisse
+            console.log(result);
+            res.status(201).json({
+                message: 'Handling POST request of the /users',
+                user: result
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.get('/:userID', (req, res, next) => {
     const id = req.params.userID;
     console.log(id);
     User.findById(id)
-    .exec()
-    .then(doc => {
-        console.log("From database", doc);
-        res.status(200).json(doc);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({error : err})
-    });
+        .exec()
+        .then(doc => {
+            console.log("From database", doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({ message: 'No valid entry found for provided ID' });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err })
+        });
 
     /* Usuado no aprendizado
     const id = req.params.userID;
-
+ 
     if (id === 'admin') {
         res.status(200).json({
             message: 'You are the Admin!',
@@ -57,20 +82,54 @@ router.get('/:userID', (req, res, next) => {
     */
 });
 
-router.patch('/:userID', (req, res, next) => {
+router.patch('/:userID', (req, res, next) => { // update com mongoose
     const id = req.params.userID;
+    const updateOps = {};
+    for( const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+    User.update({ _id: id}, { $set: updateOps })
+    .exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+    /*const id = req.params.userID;
     //find and update an user by id
     res.status(200).json({
         message: 'User updated!'
-    });
+    });*/
 });
+
+//Com patch(update), deve vir valores com as devidas propriedades como no exemplo abaixo
+//[
+//	{"propName" : "name", "value": "cabomaldade2"}
+//]
 
 router.delete('/:userID', (req, res, next) => {
     const id = req.params.userID;
+    User.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+    /*const id = req.params.userID;
     //find and delete an use by id
     res.status(200).json({
         message: 'User deleted!'
-    });
+    });*/
 });
 
 
